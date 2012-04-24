6c617d551fed329bbb769209148f613c94006b91
Open Aphid <openaphid@gmail.com>
2012-04-24 08:31:42 -0700

    initial import of v0.1

---
+++ TankBenchmarks/TankBenchmarks/OAAppDelegate.h
@@ -0,0 +1,15 @@
+/*
+ @oa_license
+ */
+
+#import <UIKit/UIKit.h>
+
+@class OARootViewController;
+
+@interface OAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) OARootViewController *viewController;
+
+@end
---
+++ TankBenchmarks/TankBenchmarks/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TankBenchmarks
+//
+//  Created by Bo Hu on 4/29/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "OAAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([OAAppDelegate class]));
+	}
+}
