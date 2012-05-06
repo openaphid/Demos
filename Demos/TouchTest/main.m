5b967f09890335608212fed4749d20886da2f300
Open Aphid <openaphid@gmail.com>
2012-05-06 21:41:08 +0800

    Adds TouchTest

---
+++ Demos/TouchTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TouchTest
+//
+//  Created by Bo Hu on 5/6/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "OAAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([OAAppDelegate class]));
+	}
+}
